dd0432c597764875a356187755b4c336bc33c62f
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-26 08:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+document.addEventListener('DOMContentLoaded',function() {
+	const fnameInput = document.getElementById('fname');
+	fnameInput.addEventListener('blur', function(){
+		fnameInput.value = fnameInput.value.toUpperCase();
+	});
+});
